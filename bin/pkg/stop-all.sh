c2f1dcc4f24d91dec4eff9d86f89c232f8c11096
Hongliang Xu <hongxu@uber.com>
2017-07-22 00:12:16 -0700

    update stop-all.sh

--- bin/pkg/stop-all.sh
+++ bin/pkg/stop-all.sh
@@ -3,28 +3,25 @@
 echo "EXECUTING: stop controller"
 PID1=`pgrep -f "Dapp_name=uReplicator-Controller"` || true
 if [ -n "$PID1" ]; then
-    kill -9 ${PID1}
+  kill -9 ${PID1}
 fi
 
 echo "EXECUTING: stop worker"
 PID2=`pgrep -f "Dapp_name=uReplicator-Worker"` || true
 if [ -n "$PID2" ]; then
-    kill -9 ${PID2}
+  kill -9 ${PID2}
 fi
 
 echo "EXECUTING: stop producing"
 PID3=`pgrep -f "./bin/produce-data-to-kafka-topic-dummyTopic.sh"` || true
 if [ -n "$PID3" ]; then
-    kill -9 ${PID3}
+  kill -9 ${PID3}
 fi
 
 echo "EXECUTING: stop consuming"
 PID4=$(ps ax | grep -i 'kafka.tools.ConsoleConsumer' | grep java | grep -v grep | awk '{print $1}')
 if [ -n "$PID4" ]; then
-  for i in "${PID4[@]}"
-  do
-      kill -9 ${i}
-  done
+  kill -9 ${PID4}
 fi
 
 bin/grid stop all
