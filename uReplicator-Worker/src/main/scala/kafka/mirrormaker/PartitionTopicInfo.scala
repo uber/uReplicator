59d6256bd098bad5bb4ed7e7b5875c5389dc4443
Hongliang Xu <hongxu@uber.com>
2017-07-25 13:58:10 -0700

    Update license

--- uReplicator-Worker/src/main/scala/kafka/mirrormaker/PartitionTopicInfo.scala
+++ uReplicator-Worker/src/main/scala/kafka/mirrormaker/PartitionTopicInfo.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015-2016 Uber Technology Inc. (streaming-core@uber.com)
+ * Copyright (C) 2015-2017 Uber Technology Inc. (streaming-data@uber.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
