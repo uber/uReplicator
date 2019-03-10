564510fba3c033155fef276d77145003e72e1e76
Yang Yang <yayang@uber.com>
2019-03-09 21:21:30 -0800

    backport 1-1 topic partition mapping

--- uReplicator-Worker/src/main/scala/kafka/mirrormaker/HelixWorkerOnlineOfflineStateModelFactory.scala
+++ uReplicator-Worker/src/main/scala/kafka/mirrormaker/HelixWorkerOnlineOfflineStateModelFactory.scala
@@ -27,7 +27,8 @@ import org.apache.helix.participant.statemachine.{StateModel, StateModelFactory}
  * @param connector
  */
 class HelixWorkerOnlineOfflineStateModelFactory(final val instanceId: String,
-                                                final val connector: KafkaConnector) extends StateModelFactory[StateModel] with Logging {
+                                                final val connector: KafkaConnector,
+                                                final val topicPartitionCountObserver: TopicPartitionCountObserver) extends StateModelFactory[StateModel] with Logging {
   override def createNewStateModel( partitionName:String) =  new OnlineOfflineStateModel(instanceId, connector)
 
   // register mm instance
@@ -40,6 +41,9 @@ class HelixWorkerOnlineOfflineStateModelFactory(final val instanceId: String,
 
       // add topic partition on the instance
       connectors.addTopicPartition(message.getResourceName, message.getPartitionName.toInt)
+      if (topicPartitionCountObserver != null) {
+        topicPartitionCountObserver.addTopic(message.getResourceName)
+      }
       debug("Finish OnlineOfflineStateModel.onBecomeOnlineFromOffline for topic: "
         + message.getResourceName() + ", partition: " + message.getPartitionName()
         + " to instance: " + instanceId)
