3322cd5a12286e44983aa421f9cfcab510b2d1c9
Yang Yang <yayang@uber.com>
2019-03-20 12:20:24 -0700

    update license header

--- uReplicator-Worker/src/main/scala/kafka/mirrormaker/HelixWorkerOnlineOfflineStateModelFactory.scala
+++ uReplicator-Worker/src/main/scala/kafka/mirrormaker/HelixWorkerOnlineOfflineStateModelFactory.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015-2017 Uber Technologies, Inc. (streaming-data@uber.com)
+ * Copyright (C) 2015-2019 Uber Technologies, Inc. (streaming-data@uber.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
