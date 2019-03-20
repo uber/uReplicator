98d81aa8f71f9fc1ffd382385bc65f267a68162d
Yang Yang <yayang@uber.com>
2019-03-20 12:20:24 -0700

    update license header

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2015-2017 Uber Technologies, Inc. (streaming-data@uber.com)
+# Copyright (C) 2015-2019 Uber Technologies, Inc. (streaming-data@uber.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
