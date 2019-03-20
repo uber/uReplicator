c7d45159085c5e2234e4bef464885a2a0e5dd939
Yang Yang <yayang@uber.com>
2019-03-20 12:20:24 -0700

    update license header
    Reviewers: yupeng, hongxu
    
    Reviewed By: yupeng, hongxu
    
    Subscribers: O2693 User yupeng: Add subscribers
    
    Differential Revision: https://code.uberinternal.com/D2631427

--- uReplicator-Manager/src/main/java/com/uber/stream/kafka/mirrormaker/manager/core/AdminHelper.java
+++ uReplicator-Manager/src/main/java/com/uber/stream/kafka/mirrormaker/manager/core/AdminHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2017 Uber Technologies, Inc. (streaming-data@uber.com)
+ * Copyright (C) 2015-2019 Uber Technologies, Inc. (streaming-data@uber.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.uber.stream.kafka.mirrormaker.manager.core;
 
 import com.alibaba.fastjson.JSON;
